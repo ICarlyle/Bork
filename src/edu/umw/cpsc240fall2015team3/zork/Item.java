83c52e4de3196d193a27657bc7cf04ec99da5fb8
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-06 18:49:07 -0500

    Completed Item's javadoc segments.

--- src/edu/umw/cpsc240fall2015team3/zork/Item.java
+++ src/edu/umw/cpsc240fall2015team3/zork/Item.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+@author Robert Jamal Washington
+*/
+/**
+Represents objects that can be found in rooms and be interacted with by the player.
+*/
 public class Item {
 
     static class NoItemException extends Exception {}
@@ -18,11 +24,23 @@ public class Item {
     private Hashtable<String,String[]> messages;
 
 
+		/**Initializes this hlnkItem}'s container variables.
+		*/
     private void init() {
         messages = new Hashtable<String,String[]>();
         aliases = new ArrayList<String>();
     }
 
+		
+    /** Returns an hlnkItem} object read from the "Items" section of a .zork file by a Scanner.
+        @param s The Scanner that reads the .zork file.
+
+        @throws NoItemException The reader object is not positioned at the
+        start of an hlnkItem} entry. A side effect of this is the reader's cursor
+        is now positioned one line past where it was.
+        @throws IllegalDungeonFormatException A structural problem with the
+        dungeon file itself, detected when trying to read this room.
+		*/
     Item(Scanner s) throws NoItemException,
         Dungeon.IllegalDungeonFormatException {
 
@@ -62,21 +80,35 @@ public class Item {
         }
     }
 
+		/**Returns whether or not this hlnkItem} has a certain primary or secondary name.
+
+		@param name The alias to compare to the hlnkItem}'s primary and secondary names.
+		*/
     boolean goesBy(String name) {
         return primaryName.equals(name) || aliases.contains(name);
     }
 
+		/**Returns the primary name of this hlnkItem}.
+		*/
     String getPrimaryName() { return primaryName; }
 
+		/**Returns a message associated with an action the player takes with this hlnkItem}.
+
+		@param verb The action the player uses with this hlnkItem}.
+		*/
     public String getMessageForVerb(String verb) {
         String[] possibleMessages = messages.get(verb);
         return possibleMessages[rng.nextInt(possibleMessages.length)];
     }
 
+		/**Returns a String that represents this hlnkItem}.
+		*/
     public String toString() {
         return primaryName;
     }
 
+		/**Returns the carrying weight of this hlnkItem}.
+		*/
     public int getWeight() {
         return weight;
     }
