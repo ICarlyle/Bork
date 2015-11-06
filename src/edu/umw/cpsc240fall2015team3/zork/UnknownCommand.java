71b7578a0d6c24d5c9286f9fb82b7c951be6ea4e
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-06 17:56:06 -0500

    Add javadoc to UnknownCommand

--- src/edu/umw/cpsc240fall2015team3/zork/UnknownCommand.java
+++ src/edu/umw/cpsc240fall2015team3/zork/UnknownCommand.java
@@ -1,14 +1,22 @@
-
+/**
+@author Ian
+*/
 package edu.umw.cpsc240fall2015team3.zork;
-
+/**
+This class will store the command the player enteres and will tell the player the command is unknown when the execute method is called.
+*/
 class UnknownCommand extends Command {
 
     private String bogusCommand;
-
+/**
+Stores the command the player entered
+*/
     UnknownCommand(String bogusCommand) {
         this.bogusCommand = bogusCommand;
     }
-
+/**
+Returns a String that states the command the player entered is not understood
+*/
     String execute() {
         return "I'm not sure what you mean by \"" + bogusCommand + "\".\n";
     }
