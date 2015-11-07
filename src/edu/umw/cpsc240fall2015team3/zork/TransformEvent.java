562fbfa0cabbbc2ba5ecd7e6a6e6d4933b0c308c
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-06 22:55:43 -0500

    Added TransformEvent javadoc

--- src/edu/umw/cpsc240fall2015team3/zork/TransformEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/TransformEvent.java
@@ -1,17 +1,26 @@
 
-
 package edu.umw.cpsc240fall2015team3.zork;
 
 /**
 @author Robert Jamal Washington
 */
 /**
-
+Changes an {@link edu.umw.cpsc240fall2015team3.zork.Item} into a different {@link edu.umw.cpsc240fall2015team3.zork.Item}.
 */
-abstract class TransformEvent {
+class TransformEvent extends Event{
+		/**Returns a {@link edu.umw.cpsc240fall2015team3.zork.TransformEvent} that uses two specific {@link edu.umw.cpsc240fall2015team3.zork.Item}s.
+
+		@param oldItemName The {@link edu.umw.cpsc240fall2015team3.zork.Item} that is being transformed.
+		@param newItemName The {@link edu.umw.cpsc240fall2015team3.zork.Item} that the old {@link edu.umw.cpsc240fall2015team3.zork.Item} is being transformed into.
+		*/	
+		TransformEvent(String oldItemName, String newItemName){
 		
-		/**
+		}
+	
+		/**Changes one {@link edu.umw.cpsc240fall2015team3.zork.Item} into a different {@link edu.umw.cpsc240fall2015team3.zork.Item}.
 		*/
-    abstract String execute();
+    public String execute(){
+
+		}
 
 }
