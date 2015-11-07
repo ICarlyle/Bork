ad110f185f882f8237401671efa9077b67edff4b
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-06 21:29:33 -0500

    Add waitCommand Javadoc

---
+++ src/edu/umw/cpsc240fall2015team3/zork/WaitCommand.java
@@ -0,0 +1,18 @@
+/**
+@author Ian
+*/
+package edu.umw.cpsc240fall2015team3.zork;
+/**
+*/
+class WaitCommand extends Command {
+
+int wait;
+/**
+Stores how much time should pass
+*/
+    WaitCommand(int wait){}
+/**
+Updates gametime by stored amount
+*/
+    public String execute(){}
+}
