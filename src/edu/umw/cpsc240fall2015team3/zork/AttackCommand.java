e0c1cc76f9fe9883a56d74fca950ed5b5c311b52
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-06 22:55:39 -0500

    Added Attack command Javadoc

---
+++ src/edu/umw/cpsc240fall2015team3/zork/AttackCommand.java
@@ -0,0 +1,18 @@
+/**
+@author Ian
+*/
+package edu.umw.cpsc240fall2015team3.zork;
+/**
+This class will search a room for an enemy named the passed argument then call methods to simulate fighting.
+*/
+class  AttackCommand extends Command {
+    private String enemy;
+/**
+Stores the name of enemy to attack
+*/
+    AttackCommand(String Enemy){}
+/**
+Returns a string that states how much damage was done
+*/
+    String execute() {}
+}
