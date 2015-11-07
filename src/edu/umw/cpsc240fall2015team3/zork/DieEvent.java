5b1d7b7e3f5904f4e5fb628850bc2e54fe0cc7a9
Alec Carlyle <acarlyle@mail.umw.edu>
2015-11-06 22:21:44 -0500

    Add javaDoc

---
+++ src/edu/umw/cpsc240fall2015team3/zork/DieEvent.java
@@ -0,0 +1,19 @@
+/**
+@author Alec
+*/
+
+package edu.umw.cpsc240fall2015team3.zork;
+/**
+The DieEvent class is an extension of the Event class used for when the player is killed off from either an item Command or by having their health drop to 0 or less. 
+*/
+class DieEvent extends Event{
+	DieEvent(){
+
+	}
+/**
+Returns a String informing the player that they are dead.
+*/
+	public String execute(){
+
+	}
+}
---
+++ src/edu/umw/cpsc240fall2015team3/zork/WinEvent.java
@@ -0,0 +1,18 @@
+/**
+@author Alec
+*/
+
+package edu.umw.cpsc240fall2015team3.zork;
+/**
+The WinEvent class is an extension of the Event class used for when the player has succesfully beaten the game.  After this command is executed the system will exit.  Will be tied to an item command.
+*/
+class WinEvent extends Event{
+	WinEvent(){
+	}
+/**
+Returns a String informing the player that they have won.
+*/
+	public String execute(){
+		
+	}
+}
