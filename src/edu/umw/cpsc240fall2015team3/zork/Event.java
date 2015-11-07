a7cd2d819eccda325afaeb59ce079c14791ce0b5
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-06 21:41:23 -0500

    Created files for Event.java and its subclasses

---
+++ src/edu/umw/cpsc240fall2015team3/zork/Event.java
@@ -0,0 +1,16 @@
+
+package edu.umw.cpsc240fall2015team3.zork;
+
+/**
+@author Robert Jamal Washington
+*/
+/**
+
+*/
+abstract class Event {
+		
+		/**
+		*/
+    abstract String execute();
+
+}
---
+++ src/edu/umw/cpsc240fall2015team3/zork/UnlockEvent.java
@@ -0,0 +1,17 @@
+
+
+package edu.umw.cpsc240fall2015team3.zork;
+
+/**
+@author Robert Jamal Washington
+*/
+/**
+
+*/
+abstract class UnlockEvent {
+		
+		/**
+		*/
+    abstract String execute();
+
+}
