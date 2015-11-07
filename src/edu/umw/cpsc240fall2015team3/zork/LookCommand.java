b8ea9f3d135b5aebf1e1c5de32248c65d591a477
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-06 20:59:00 -0500

    Updated Look+Movement Command JavaDOC

--- src/edu/umw/cpsc240fall2015team3/zork/LookCommand.java
+++ src/edu/umw/cpsc240fall2015team3/zork/LookCommand.java
@@ -3,14 +3,14 @@
 */
 package edu.umw.cpsc240fall2015team3.zork;
 /**
-When executed, this command displays the room text as if the player never entered the current room
+This class is an extension of the Command class and is used for displaying the room text as if the player never entered the current room
 */
 class LookCommand extends Command {
 
     LookCommand() {
     }
 /**
-Desribes the current room as if it is the first time the player entered the room.
+Returns the desription of the current room as if it is the first time the player entered the room.
 */
     public String execute() {
         return GameState.instance().getAdventurersCurrentRoom().
--- src/edu/umw/cpsc240fall2015team3/zork/MovementCommand.java
+++ src/edu/umw/cpsc240fall2015team3/zork/MovementCommand.java
@@ -1,15 +1,23 @@
-
+/**
+@author Ian
+*/
 package edu.umw.cpsc240fall2015team3.zork;
-
+/**
+This class is an extension of the Command class and is used for transfering the player between rooms.
+*/
 class MovementCommand extends Command {
 
-    private String dir;
-                       
-
+    private String dir;             
+/**
+Stores the direction the player enters
+*/
     MovementCommand(String dir) {
         this.dir = dir;
     }
-
+/**
+Finds a room from an exit with the direction the player entered, sets that room as current, and return a string stating if the the player can or cannot go to that exit. 
+If the movement command does not go to a room, returns a string desribing how the player can't go that direction
+*/
     public String execute() {
         Room currentRoom = GameState.instance().getAdventurersCurrentRoom();
         Room nextRoom = currentRoom.leaveBy(dir);
