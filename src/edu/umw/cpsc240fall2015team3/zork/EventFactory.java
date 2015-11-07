61648f0e3528dd437b041e09801dcf96ebc93c3a
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-06 21:51:11 -0500

    Fixed glitch in EventFactory package name

--- src/edu/umw/cpsc240fall2015team3/zork/EventFactory.java
+++ src/edu/umw/cpsc240fall2015team3/zork/EventFactory.java
@@ -2,7 +2,7 @@
 @author Alec
 */
 
-package edu.umw.cpsc240fall2015team3.zork
+package edu.umw.cpsc240fall2015team3.zork;
 /**
 The singleton EventFactory class instantiates unique Event objects that are derived from item-tied Commands.  New event objects can be instantiated inside of Commands.  Events are directly tied with {@link edu.umw.cpsc240fall2015team3.zork.GameState} as they directly affect a player's interaction with the dungeon.   
 */
