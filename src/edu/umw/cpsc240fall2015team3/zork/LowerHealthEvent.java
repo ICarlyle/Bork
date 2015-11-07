e8e71efbd87750cd3787b034f950c950d7cf9d11
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-06 22:33:01 -0500

    Added LowerHealthEvent javadoc

--- src/edu/umw/cpsc240fall2015team3/zork/LowerHealthEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/LowerHealthEvent.java
@@ -1,17 +1,25 @@
 
-
 package edu.umw.cpsc240fall2015team3.zork;
 
 /**
 @author Robert Jamal Washington
 */
 /**
-
+Changes the player's health by a specified value.
 */
-abstract class LowerHealthEvent {
+class LowerHealthEvent extends Event{
+		/**Returns a {@link edu.umw.cpsc240fall2015team3.zork.LowerHealthEvent} this uses a certain damage value.
+
+		@param damage The amount that the player's health should be lowered by.
+		*/	
+		LowerHealthEvent(int damage){
 		
-		/**
+		}
+	
+		/**Lowers the player's health by a specified amount. This simulatneously acts as a way to increase the player's health by passing in a positive value for the damage.
 		*/
-    abstract String execute();
+    public String execute(){
+
+		}
 
 }
