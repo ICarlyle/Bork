a1f1b997505bd3ea2578103ff3d6865647347f5a
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-06 20:19:44 -0500

    Added Command javadocs

--- src/edu/umw/cpsc240fall2015team3/zork/Command.java
+++ src/edu/umw/cpsc240fall2015team3/zork/Command.java
@@ -1,8 +1,16 @@
 
 package edu.umw.cpsc240fall2015team3.zork;
 
+/**
+@author Robert Jamal Washington
+*/
+/**
+Performs an action on the {@link edu.umw.cpsc240fall2015team3.zork.GameState}.
+*/
 abstract class Command {
-
+		
+		/**Performs an action that affects the {@link edu.umw.cpsc240fall2015team3.zork.GameState} and returns a message pertaining to that action.
+		*/
     abstract String execute();
 
 }
