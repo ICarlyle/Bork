2dae70053019800070ade2f76eaa954e1d2bbde2
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-08 23:16:58 -0500

    Score Error fix

--- src/edu/umw/cpsc240fall2015team3/zork/ShowScoreCommand.java
+++ src/edu/umw/cpsc240fall2015team3/zork/ShowScoreCommand.java
@@ -9,7 +9,7 @@ class ShowScoreCommand extends Command{
 		String rank = "";
 		if (score <= 0){rank = "Hungry Hungry Hoarder";}
 		if (score > 0 && score <= 10){rank = "Howling Scrounger";}
-		if (score > 10 $$ score <= 20){rank = "Sluggish Stasher";}
+		if (score > 10 && score <= 20){rank = "Sluggish Stasher";}
 		if (score > 20 && score <= 30){rank = "Circumstantial Conglomerator";}
 		if (score > 30 && score <= 40){rank = "Bountiful Baron";}
 		if (score > 40){rank = "The Undying";}
