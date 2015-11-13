a50741a808efc5c339f446d47e55a6128f183aca
Alec Carlyle <acarlyle@mail.umw.edu>
2015-11-13 17:24:17 -0500

    Add AddScore

--- src/edu/umw/cpsc240fall2015team3/zork/AddScoreEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/AddScoreEvent.java
@@ -9,18 +9,26 @@ package edu.umw.cpsc240fall2015team3.zork;
 Changes the players score by the passed value.
 */
 class AddScoreEvent extends Event {
+
+	private int points;
 		
 		/**
 		Constructs a {@link edu.umw.cpsc240fall2015team3.zork.AddScoreEvent} object that uses the passed score value.
 		*/
       		AddScoreEvent(int points){
+			this.points = points;
+		}
+
+		AddScoreEvent(){
+			this.points = 0;
 		}
 /**
 Raises the player's score by the specified amount.
 @return a corresponding String message
 */	
 	String execute(){
-		return "";	
+		GameState.instance().addToAdventurersScoreBy(points);
+		return "You earned " + points + " points!\n";	
 	}
 
 }
