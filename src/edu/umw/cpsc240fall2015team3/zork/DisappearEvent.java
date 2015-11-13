e1904d079ff06a06b265a074eb9ff297a3c65837
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-13 00:47:02 -0500

    DisappearEvent finished(?)

--- src/edu/umw/cpsc240fall2015team3/zork/DisappearEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/DisappearEvent.java
@@ -14,14 +14,20 @@ class DisappearEvent extends Event{
 		/**
 		Constructs a {@link edu.umw.cpsc240fall2015team3.zork.DisappearEvent} object that will remove a specified item from {@link edu.umw.cpsc240fall2015team3.zork.GameState} and the {@link edu.umw.cpsc240fall2015team3.zork.Dungeon} object.
 		*/
-		DisappearEvent(String name){
-			itemName = name;
+		DisappearEvent(String itemName){
+			this.itemName = itemName;
 		}
 /**
 Completely removes the passed item from the current Dungeon and GameState.
 */
     String execute(){
-    return "";
+	try{
+	    Item theItem = GameState.instance().getItemFromInventoryNamed(itemName);
+	    GameState.instance().removeFromInventory(theItem);
+	    return itemName+" ";
+	}
+	catch(Item.NoItemException e) {
+	    return "You don't have a "+itemName+".\n";
+	}
     }
-
 }
