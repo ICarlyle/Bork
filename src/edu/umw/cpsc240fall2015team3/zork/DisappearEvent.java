9ebb96396eb30a4b2400ca5464c314800a716a20
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-12 22:25:22 -0500

    Semicolon fix

--- src/edu/umw/cpsc240fall2015team3/zork/DisappearEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/DisappearEvent.java
@@ -10,7 +10,7 @@ Removes a certain item from the GameState and Dungeon.
 */
 class DisappearEvent extends Event{
 
-	private String itemName
+	private String itemName;
 		/**
 		Constructs a {@link edu.umw.cpsc240fall2015team3.zork.DisappearEvent} object that will remove a specified item from {@link edu.umw.cpsc240fall2015team3.zork.GameState} and the {@link edu.umw.cpsc240fall2015team3.zork.Dungeon} object.
 		*/
