2da7e85c61b98aeb65800097eb984e4a83c4fcff
Alec Carlyle <acarlyle@mail.umw.edu>
2015-11-13 23:34:34 -0500

    Fix Wound event

--- src/edu/umw/cpsc240fall2015team3/zork/EventFactory.java
+++ src/edu/umw/cpsc240fall2015team3/zork/EventFactory.java
@@ -42,17 +42,17 @@ Returns an Event object based on the passed String that will directly influence 
 	eventName = itemAndEvent[0];
 	item = itemAndEvent[1];
 	}
-	if (eventName.contains("(")){
-		String[] verb = item.split("\\(");
-		System.out.println("verb[0] " + verb[0]);
-		System.out.println("verb[1] " + verb[1]);
+	if (event.contains("(")){
+		String[] verb = event.split("\\(");
+		//System.out.println("verb[0] " + verb[0]);
+		//System.out.println("verb[1] " + verb[1]);
 		verb[1].replace(")", "").trim();
 	
 	if (event.contains("1") || event.contains("2") || event.contains("3")
 	|| event.contains("4") || event.contains("5") || event.contains("6")
 	|| event.contains("7") || event.contains("8") || event.contains("9")
 	|| event.contains("0")){
-		numberVal = Integer.parseInt(verb[1]);
+		numberVal = Integer.parseInt(verb[1].replace(")", "").trim());
 		}
 	else{
 		param = eventName;
@@ -70,7 +70,9 @@ Returns an Event object based on the passed String that will directly influence 
 	if(event.contains("Score")){return new AddScoreEvent(numberVal);}
 	if(event.contains("Die")){return new DieEvent();}
 	if(event.contains("Disappear")){return new DisappearEvent(item);}
-	if(event.contains("Wound")){return new LowerHealthEvent(numberVal);}
+	if(event.contains("Wound")){
+	System.out.println(numberVal);
+	return new LowerHealthEvent(numberVal);}
 	if(event.contains("Teleport")){return new TeleportEvent();}
 	if(event.contains("Transform")){return new TransformEvent(param1,param2);}
 	if(event.contains("Win")){return new WinEvent();}
