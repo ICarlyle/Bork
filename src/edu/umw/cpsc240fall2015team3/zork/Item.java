3e7c62bb487439c46de78ed2a5501ae6196e1bc5
Robert Washington <rwashin2@rosemary@umw.edu>
2015-11-13 20:02:15 -0500

    Completed TrEvent; Added setters to Item

--- src/edu/umw/cpsc240fall2015team3/zork/Item.java
+++ src/edu/umw/cpsc240fall2015team3/zork/Item.java
@@ -164,6 +164,14 @@ System.out.println("\nVERB:'"+verb+"' | EVENTS:'"+eventSegment+"'");
 	return events;
     }
 		
+		public ArrayList<String> getAliases(){
+			return aliases;
+		}
+
+		public Hashtable<String, String> getMessages(){
+			return messages;
+		}
+
 		public void setAliases(ArrayList<String> theAliases){
 			aliases = theAliases;
 		}
