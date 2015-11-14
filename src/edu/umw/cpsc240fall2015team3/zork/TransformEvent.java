b5da40d31ecd12c4ac80500cd5ccc80683e55781
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-14 06:29:38 -0500

    Tiny JavaDoc Changes

--- src/edu/umw/cpsc240fall2015team3/zork/TransformEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/TransformEvent.java
@@ -9,31 +9,31 @@ Changes an {@link edu.umw.cpsc240fall2015team3.zork.Item} into a different {@lin
 */
 class TransformEvent extends Event{
 
-		private String oldItemName, newItemName;
+	private String oldItemName, newItemName;
 
-		/**Returns a {@link edu.umw.cpsc240fall2015team3.zork.TransformEvent} that uses two specific {@link edu.umw.cpsc240fall2015team3.zork.Item}s.
+	/**Returns a {@link edu.umw.cpsc240fall2015team3.zork.TransformEvent} that uses two specific {@link edu.umw.cpsc240fall2015team3.zork.Item}s.
 
-		@param oldItemName The {@link edu.umw.cpsc240fall2015team3.zork.Item} that is being transformed.
-		@param newItemName The {@link edu.umw.cpsc240fall2015team3.zork.Item} that the old {@link edu.umw.cpsc240fall2015team3.zork.Item} is being transformed into.
-		*/	
-		TransformEvent(String oldItemName, String newItemName){
-			this.oldItemName = oldItemName;
-			this.newItemName = newItemName;	
-		}
+	@param oldItemName The {@link edu.umw.cpsc240fall2015team3.zork.Item} that is being transformed.
+	@param newItemName The {@link edu.umw.cpsc240fall2015team3.zork.Item} that the old {@link edu.umw.cpsc240fall2015team3.zork.Item} is being transformed into.
+	*/	
+	TransformEvent(String oldItemName, String newItemName){
+		this.oldItemName = oldItemName;
+		this.newItemName = newItemName;	
+	}
 	
-		/**Changes one {@link edu.umw.cpsc240fall2015team3.zork.Item} into a different {@link edu.umw.cpsc240fall2015team3.zork.Item}.
-		*/
-/**
-    public String execute(){/
-			Item oldItem = GameState.instance().getItemInVicinityNamed(oldItemName);
-			Item newItem = GameState.instance().getDungeon.getItem(newItemName);
+	/**Changes one {@link edu.umw.cpsc240fall2015team3.zork.Item} into a different {@link edu.umw.cpsc240fall2015team3.zork.Item}.
+	*/
+	/**
+	public String execute(){/
+		Item oldItem = GameState.instance().getItemInVicinityNamed(oldItemName);
+		Item newItem = GameState.instance().getDungeon.getItem(newItemName);
 
-			oldItem.setAliases(newItem.getAliases());
-			oldItem.setEvents(newItem.getEvents());
-			oldItem.setMessages(newItem.getMessages());
-			oldItem.setPrimaryName(newItem.getPrimaryName());
-			oldItem.setWeight(newItem.getWeight());
-			return "The " + oldItemName + "has become " + newItemName + ".\n";
+		oldItem.setAliases(newItem.getAliases());
+		oldItem.setEvents(newItem.getEvents());
+		oldItem.setMessages(newItem.getMessages());
+		oldItem.setPrimaryName(newItem.getPrimaryName());
+		oldItem.setWeight(newItem.getWeight());
+		return "The " + oldItemName + "has become " + newItemName + ".\n";
 		}
 */
 	public String execute(){
