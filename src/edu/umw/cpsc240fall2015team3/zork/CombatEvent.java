b03b8098cdc5f659826161fd6f4968f90f7e1454
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-23 18:38:35 -0500

    Comba Event For Loop Changed

--- src/edu/umw/cpsc240fall2015team3/zork/CombatEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/CombatEvent.java
@@ -25,22 +25,37 @@ class CombatEvent extends Event {
 		System.out.println(chosenEnemy.talk());
 		int playerStr = GameState.instance().getStr();
 		int playerDef = GameState.instance().getDef();
-		
+		int npcStr;int npcDef;int npcHp;
 		while (!enemies.isEmpty()){
-			for (Npc npc : enemies){
-				int npcStr = npc.strength();
-				int npcDef = npc.defense();
-				int npcHp = npc.health();
-				//System.out.println(npc.drop());
-				npc.talk();
-				npc.wound(10000000);
-				if (npc.health() <= 0){
-					System.out.println("kill");
-					System.out.println(npc.drop());
-					//enemies.remove(npc);
-					currRoom.remove(npc);
-					//GameState.instance().removeNpc(npc);
+			int enemyNum = enemies.size();
+			for (int i =0; i<enemyNum;){
+                                npcStr = enemies.get(i).strength();
+                                npcDef = enemies.get(i).defense();
+                                npcHp = enemies.get(i).health();
+                                //System.out.println(npc.drop());
+                                enemies.get(i).talk();
+                                enemies.get(i).wound(10000000);
+                                if (enemies.get(i).health() <= 0){
+                                        System.out.println("kill");
+                                        System.out.println(enemies.get(i).drop());
+                                        //enemies.remove(enemies.get(i));
+                                        currRoom.remove(enemies.get(i));
+                                        //GameState.instance().removeNpc(npc);
+                                i++;
 				}
+//				int npcStr = npc.strength();
+//				int npcDef = npc.defense();
+//				int npcHp = npc.health();
+//				//System.out.println(npc.drop());
+//				npc.talk();
+//				npc.wound(10000000);
+//				if (npc.health() <= 0){
+//					System.out.println("kill");
+//					System.out.println(npc.drop());
+//					//enemies.remove(npc);
+//					currRoom.remove(npc);
+//					//GameState.instance().removeNpc(npc);
+//				}
 			}	
 			break;	
 		}
