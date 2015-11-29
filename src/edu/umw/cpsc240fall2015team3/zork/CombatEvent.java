e9366c30c20a528b05c1cd609c5199f63b2853e5
Ian Carlyle <Icarlyle@mail.umw.edu>
2015-11-29 03:37:05 -0500

    Add combatStats printed

--- src/edu/umw/cpsc240fall2015team3/zork/CombatEvent.java
+++ src/edu/umw/cpsc240fall2015team3/zork/CombatEvent.java
@@ -25,39 +25,43 @@ class CombatEvent extends Event {
 		int playerStr = GameState.instance().getStr();
 		int playerDef = GameState.instance().getDef();
 		playerStr=20;
-		int npcStr;int npcDef;int npcHp;int npcDam=0;
+		int npcStr;int npcDef;int npcHp;int npcAtt=0;int playerAtt=0;
+		if(playerDef<=0){playerDef=1;}
 		if (!enemies.isEmpty()){
 			enemies = currRoom.getEnemies();
 			int enemyNum = enemies.size();
 			for(Npc npc : enemies){
 				npcStr = npc.strength();
-				npcDef = npc.defense()+1;
+				npcDef = npc.defense();
 				npcHp = npc.health();
+System.out.println("\n\n===ENEMY===ENEMY===ENEMY===");
 System.out.println("ENEMY ENCOUNTER: "+npc.getName());
 System.out.println("     HP: "+npcHp+" Def: "+npcDef+" Str: "+npcStr);
 				System.out.println(npc.talk());
 /*Enemy Attack*/ 		
-				npcDam = npcStr;
-GameState.instance().subtractAdventurersHealthBy(npcDam);
-EventFactory.instance().parse("Wound").execute();
+				npcAtt = (npcStr*10)/playerDef;
 
+System.out.println("The Player takes "+npcAtt+" damage.");
+GameState.instance().subtractAdventurersHealthBy(npcAtt);
+EventFactory.instance().parse("Wound").execute();
 /*Attack the Enemy */		if(npc.getName().equals(enemyName)){
 				npc.wound(playerStr);
-				
-					if (npc.health() <= 0){
-					System.out.println(enemyName+" killed");
-					System.out.println(npc.drop());
-					//enemies.remove(npc);
-					currRoom.remove(npc);
-					//GameState.instance().removeNpc(npc);
-				}
-				}
+				playerAtt = (playerStr*10)/npcDef;
 
+					if (npc.health() <= 0){
+					
+//					System.out.println(enemyName+" killed");
+//					System.out.println(npc.drop());
+//					//enemies.remove(npc);
+//					currRoom.remove(npc);
+//					//GameState.instance().removeNpc(npc);
+					}
+				}
 			}	
 			//break;
 			//return "YOU DEFEATED\n";
-System.out.println("The Player takes "+npcDam+" damage.");
-System.out.println("The "+enemyName+" takes "+playerStr+" damage.");
+//System.out.println("The Player takes "+npcAtt+" damage.");
+System.out.println("The "+enemyName+" takes "+playerAtt+" damage.");
 			return "";
 		}
 		return "There is no " + enemyName + " here!\n";
